92ee1e49f0193fe71de2eb1a3199b8c936bb2a16
Aaron <aaronwan2@gmail.com>
2015-07-05 22:19:24 -0400

    in class stuff

--- samplesize.R
+++ samplesize.R
@@ -1,6 +1,15 @@
+#dice simulation
 exp1 <- rowSums(replicate(2, sample(6, 10, replace=T)))
 exp2 <- rowSums(replicate(2, sample(6, 100, replace=T)))
 exp3 <- rowSums(replicate(2, sample(6, 10000, replace=T)))
+#plot the data
 hist(exp1)
 hist(exp2)
 hist(exp3)
+#cumulative distribution
+plot(ecdf(exp3))
+#normal distribution
+n <- rnorm(1000)
+hist(n)
+
+
